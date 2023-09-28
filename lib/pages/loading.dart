10c77e424f4fc510164089a8e98a6626d742de95
Kishore Kumar <kishorekumarcdckap@gmail.com>
2023-09-28 12:11:30 +0530

    Datas passed to home route by arguments

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -18,10 +18,10 @@ class _LoadingState extends State<Loading> {
         location: 'berlin', flag: 'germany.png', url: 'Europe/Berlin');
     await instance.getTime();
     // ignore: use_build_context_synchronously
-    Navigator.pushReplacementNamed(context, '/home',arguments: {
-      'location':instance.location,
-      'flag':instance.flag,
-      time:instance.time
+    Navigator.pushReplacementNamed(context, '/home', arguments: {
+      'location': instance.location,
+      'flag': instance.flag,
+      time: instance.time
     });
     print(instance.time);
 
